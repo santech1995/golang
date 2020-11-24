3b9f63e15db2c625d9e04f2c07dbf6fae99b77e0
santech1995 <sanjanac777@gmail.com>
2020-11-24 23:04:03 +0530

    ex1.go

---
+++ ex1.go
@@ -0,0 +1,21 @@
+//Simple program that illustrates the use of the major data types - 'int','string' and 'boolean'
+
+package main
+
+import "fmt"
+
+func main() {
+	x := 42
+	y := "James Bond"
+	z := true
+	fmt.Println(x, y, z)
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+}
+
+//OUTPUT:
+// 42 James Bond true
+// 42
+// James Bond
+// true
