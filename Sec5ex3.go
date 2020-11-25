6a4dd15bc319861c0b3bbe9172b9b92aead53ce5
santech1995 <sanjanac777@gmail.com>
2020-11-25 23:25:50 +0530

    Sec5ex3.go

---
+++ Sec5ex3.go
@@ -0,0 +1,18 @@
+//Simple program to illustrate the use of Sprintf
+
+package main
+
+import "fmt"
+
+var x int = 42
+var y string = "James Bond"
+var z bool = true
+
+func main() {
+	s := fmt.Sprintf("%v\t%v\t%v\t", x, y, z)
+	fmt.Println(s)
+
+}
+
+//OUTPUT:
+//42      James Bond      true
