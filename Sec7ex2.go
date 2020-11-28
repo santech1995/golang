884953c7f7dd8d952ba0419a29c375121391a49c
santech1995 <sanjanac777@gmail.com>
2020-11-28 11:09:20 +0530

    Sec7ex2.go

---
+++ Sec7ex2.go
@@ -0,0 +1,28 @@
+//Simple program to illustrate the use of comparison operators in GO
+
+package main
+
+import "fmt"
+
+func main() {
+	a := (42 == 42)
+	b := (42 <= 43)
+	c := (42 >= 43)
+	d := (42 != 43)
+	e := (42 < 43)
+	f := (42 > 43)
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
+	fmt.Println(f)
+}
+
+//OUTPUT:
+// true
+// true
+// false
+// true
+// true
+// false
