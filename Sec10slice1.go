e1e5c0c4af74d630458d81742956f533feaeae8b
santech1995 <sanjanac777@gmail.com>
2020-12-13 11:30:42 +0530

    Sec10slice1.go

---
+++ Sec10slice1.go
@@ -0,0 +1,20 @@
+//Program to implement a slice in GO
+
+package main
+
+import "fmt"
+
+func main() {
+	//aggregate => data structures having values of the same type
+	//composite literal => data structures having data of different types
+	x := []int{4, 5, 6, 7, 8}
+	fmt.Println(x)
+}
+
+//eg of aggregate data structures: arrays,slices,lists
+//eg of composite data structures: struct
+
+//a SLICE allows you to group values of the SAME TYPE
+
+//OUTPUT:
+//[4 5 6 7 8]
