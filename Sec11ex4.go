885c041204d72bd5a8f542f8214358fe4aa90664
santech1995 <sanjanac777@gmail.com>
2020-12-19 11:00:29 +0530

    Sec11ex4.go

---
+++ Sec11ex4.go
@@ -0,0 +1,28 @@
+//Program to perform append operation on arrays in GO
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	x := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
+	fmt.Println(x)
+	x = append(x, 52)
+	fmt.Println(x)
+	x = append(x, 53, 54, 55)
+	fmt.Println(x)
+	y := []int{56, 57, 58, 59, 60}
+	fmt.Println(y)
+	x = append(x, y...)
+	fmt.Println(x)
+
+}
+
+//OUTPUT:-
+// [42 43 44 45 46 47 48 49 50 51]
+// [42 43 44 45 46 47 48 49 50 51 52]
+// [42 43 44 45 46 47 48 49 50 51 52 53 54 55]
+// [56 57 58 59 60]
+// [42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60]
