be55a63f08482c68c3551b2e1a8430245150e789
santech1995 <sanjanac777@gmail.com>
2020-12-20 23:08:01 +0530

    Sec11ex5.go

---
+++ Sec11ex5.go
@@ -0,0 +1,18 @@
+//Program to slice an array in GO using the append function
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	x := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
+	fmt.Println(x)
+	x = append(x[:3], x[6:]...)
+	fmt.Println(x)
+}
+
+//OUTPUT:-
+// [42 43 44 45 46 47 48 49 50 51]
+// [42 43 44 48 49 50 51]
